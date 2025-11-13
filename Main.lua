bf024a5dd3829aa6c872747dcc09d0b716ea7f64
Roblox0294 <splurgegt@gmail.com>
2025-11-12 21:37:23 -0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -90,33 +90,6 @@ settings().Physics.AllowSleep = false
 settings().Physics.PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.Disabled
 
 --[Execute when rejoin]
-LocalPlayer.OnTeleport:Connect(function(State)
-    if State == Enum.TeleportState.InProgress and syn then
-        syn.queue_on_teleport([[
-            repeat wait() until game:IsLoaded() and game.Players.LocalPlayer.Character
-            Wait(1)
-            options = {}
-
-            options.HeadScale = 2          -- Headscale of camera (Does not change actual head size)
-            options.FakeHandsTransparency = 1  -- Transparency of Arm Hitboxes
-            options.Bubblechat = true      -- Force Bubblechat
-
-            options.PointerRange = 10      -- Range you can click buttons with your arm
-    
-            options.TurnDelay = 0.05       -- Delay in sec. for how fast you can turn left and right
-            options.TurnAngle = 15         -- Change in angle left/right (degrees)
-
-            options.ChatEnabled = true     -- See chat on your left hand in-game
-            options.ChatLocalRange = 70   -- Local chat range
- 
-            options.Hands = false          -- If you want hands in R6 (You need hats)
-            options.RightHand = "Racing Helmet Flames"
-            options.LeftHand = "Racing Helmet USA"
-            
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/saucekid/sauceVR/main/extra/ROrilla.lua"))()
-        ]])
-    end
-end)
 
 --=========[Functions]==========--
 function getModule(module)
